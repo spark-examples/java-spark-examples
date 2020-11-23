90a3491199eff003b0a30d4773d1d111946931e4
NNK <54602997+sparkcodegeeks@users.noreply.github.com>
2020-11-23 11:12:56 -0800

    Initial

---
+++ src/main/java/com/exp/Test.java
@@ -0,0 +1,15 @@
+package com.exp;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> arrL = new ArrayList<Integer>();
+        arrL.add(1);
+        arrL.add(2);
+        arrL.add(3);
+        arrL.add(4);
+        arrL.forEach(x->System.out.println(x));
+    }
+}
